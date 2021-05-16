0a7ab4c577ccd14e9facef5f6962d90d0425f3d7
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-05-16 01:43:51 -0300

    Testea que aparezca una comida listada

--- frontend/src/app/selector-comida/selector-comida.component.spec.ts
+++ frontend/src/app/selector-comida/selector-comida.component.spec.ts
@@ -3,49 +3,71 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ComidaService } from '../servicios/comida.service';
 import { SelectorComidaComponent } from './selector-comida.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { Comida } from '../modelos/comida';
-import { HttpClient } from '@angular/common/http';
+import { DiaService } from '../servicios/dia.service';
+import { ActivatedRoute } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { NuevaComidaComponent } from '../nueva-comida/nueva-comida.component';
 
 fdescribe('SelectorComidaComponent', () => {
   let component: SelectorComidaComponent;
   let fixture: ComponentFixture<SelectorComidaComponent>;
-  let comidaServiceStub: Partial<ComidaService>;
+  let obtenerComidasSpy: jasmine.Spy;
   let comidaService: ComidaService;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [ SelectorComidaComponent ],
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
       ],
       providers: [
-        SelectorComidaComponent
+        [
+          { provide: ComidaService },
+          { provide: DiaService },
+          { provida: ActivatedRoute, useValue: { queryParams: of(1)} }
+        ]
       ]
     });
 
     fixture = TestBed.createComponent(SelectorComidaComponent);
     component = fixture.componentInstance;
+
     comidaService = TestBed.inject(ComidaService);
-    fixture.detectChanges();
+    activatedRoute = TestBed.inject(ActivatedRoute);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('debe obtener 2 comidas al ejecutar ngOnInit()', () => {
+  it('debe obtener comidas al ejecutar ngOnInit()', () => {
+    //Dadas dos comidas
     component.comidas = [];
     const respuesta: Comida[] = [
       { id: 1, nombre: "Tarta", ingredientes: [] },
       { id: 2, nombre: "Milanesa", ingredientes: [] }
     ];
     spyOn(comidaService, 'obtenerComidas').and.returnValue(of(respuesta));
-
+    //Cuando inicio el componente
     component.ngOnInit();
-
+    //Entonces contiene el componente tiene dos comidas
     expect(component.comidas.length).toEqual(2);
   });
 
-});
+  it('debe renderizar una comida en la tabla', () => {
+    //Dado que tengo una sola comida
+    const comidaMock = [ {id: 1, nombre: "Sopa", ingredientes: []} ];
+    spyOn(comidaService, 'obtenerComidas').and.returnValue(of(comidaMock));
+    //Cuando inicio el componente
+    component.ngOnInit();
+    fixture.detectChanges();
+    //Entonces veo la comida
+    const comidaListada: HTMLElement = fixture.debugElement.nativeElement.querySelector('td');
+    expect(comidaListada.innerHTML.trim()).toEqual(comidaMock[0].nombre);
+  })
 
+});
