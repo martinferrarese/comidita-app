8a061eca24f0a4c020d4f433c8da1369a46fd3db
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-06-05 19:49:39 -0300

    Testea el inicio del modo edición

--- frontend/src/app/editor-comidas/editor-comidas.component.spec.ts
+++ frontend/src/app/editor-comidas/editor-comidas.component.spec.ts
@@ -1,14 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Comida } from '../modelos/comida';
 import { NuevaComidaComponent } from '../nueva-comida/nueva-comida.component';
 import { ComidaService } from '../servicios/comida.service';
 import { EditorComidasComponent } from './editor-comidas.component';
 
-fdescribe('EditorComidasComponent', () => {
+describe('EditorComidasComponent', () => {
   let component: EditorComidasComponent;
   let fixture: ComponentFixture<EditorComidasComponent>;
   let comidaService: ComidaService;
@@ -16,7 +17,7 @@ fdescribe('EditorComidasComponent', () => {
 
   beforeEach(() => {
     let activatedRouteStub: Partial<ActivatedRoute> = { queryParams: of([]) };
-    let comidaServiceStub: Partial<ComidaService> = { obtenerComida: () => {return of()}};
+    let comidaServiceStub: Partial<ComidaService> = { obtenerComida: () => {return of()}, obtenerComidas: () => {return of([])} };
 
     TestBed.configureTestingModule({
       declarations: [ EditorComidasComponent, NuevaComidaComponent ],
@@ -53,12 +54,37 @@ fdescribe('EditorComidasComponent', () => {
     // Cuando inicio el componente
     component.ngOnInit();
     fixture.detectChanges();
-    
+
+    // Entonces se listan las comidas
     const arroz: HTMLTableDataCellElement = fixture.debugElement.nativeElement.querySelector('#comida-1');
     expect(arroz.innerHTML).toContain("Arroz con pollo");
     const milanesa: HTMLTableDataCellElement = fixture.debugElement.nativeElement.querySelector('#comida-2');
     expect(milanesa.innerHTML).toContain("Milanesa con ensalada");
     expect(component.modoEdicion).toEqual(false);
     expect(component.comidaAEditar).toBeUndefined();
-  })
+  });
+
+  it('debe iniciar en modo edición cuando recibe el id de una comida por parámetro', fakeAsync(() => {
+    // Dado que se recibe un id de comida por parámetro
+    const idComida = 3;
+    const comida = new Comida();
+    comida.id = Number(idComida);
+    comida.nombre = "Puchero";
+    comida.ingredientes = [];
+    activatedRoute.queryParams = of({idComida: 3});
+    // Y que esa comida existe
+    spyOn(comidaService, 'obtenerComida').and.returnValue(of(comida).pipe(delay(1)));
+
+    // Cuando inicia el componente
+    fixture.detectChanges();
+    tick(1);
+    fixture.detectChanges();
+
+    // Entonces muestra la edición de esa comida
+    const inputNombreComida: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#nombre-comida');
+    expect(inputNombreComida.value).toEqual("Puchero");
+    expect(comidaService.obtenerComida).toHaveBeenCalled();
+    expect(component.comidaAEditar).toBe(comida);
+    expect(component.modoEdicion).toEqual(true);
+  }));
 });
