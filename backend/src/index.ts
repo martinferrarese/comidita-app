9c1fe87a806dc2579b9e3c553526c32dc8952648
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-09-13 23:16:38 -0300

    Modificación menor del index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-function sum (num1:number, num2:number){
-    return num1 + num2;
+function sum(num1: number, num2: number) {
+  return num1 + num2;
 }
-console.log(sum(8,4))
+console.log(sum(2, 2));
