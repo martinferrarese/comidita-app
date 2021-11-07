18f5a17fdde14106cae7d335eb1738b476819c10
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2021-11-07 19:33:06 -0300

    Agrega el repositorio de Comidas

--- backend/src/repositorios/comida-repository.ts
+++ backend/src/repositorios/comida-repository.ts
@@ -3,11 +3,14 @@ import { Comida } from '../modelos/comida';
 export class ComidaRepository {
   public buscarComida(id: number): Comida {
     const comida = new Comida('');
-    comida.id = id;
     return comida;
   }
 
   public buscarComidas(): Comida[] {
     return [];
+  }
+
+  public almacenarComida(nuevaComida: Comida): Comida {
+    return nuevaComida;
   }
 }
