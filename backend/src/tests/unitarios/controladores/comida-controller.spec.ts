25b356436575a2f903e674dc6f0ca755d9e96b5b
Martín Ferrarese <martin.ferrarese@grupoesfera.com.ar>
2022-01-04 01:57:20 -0300

    Deja una prueba de ejemplo funcionando

--- backend/src/tests/unitarios/controladores/comida-controller.spec.ts
+++ backend/src/tests/unitarios/controladores/comida-controller.spec.ts
@@ -5,6 +5,6 @@ import { App } from '../../../server';
 describe('Pruebas sobre ComidaController', () => {
   it('Devuelve status 200 al interactuar con /api', (done) => {
     const app = new App().app;
-    supertest(app).get('/api').expect({ message: 'Alooooo' }).expect(200, done);
+    supertest(app).get('/api').expect(200).expect({ message: 'Alooooo' }, done);
   });
 });
